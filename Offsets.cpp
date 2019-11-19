3062ee8ce3d3d166b4db205fc2e4499198bb1e30
pawREP <pawREP@users.noreply.github.com>
2019-11-19 14:32:52 +0100

    Updated offsets for client version 2.72.0

--- Offsets.cpp
+++ Offsets.cpp
@@ -3,33 +3,34 @@
 
 GameOffsets::GameVersion GameOffsets::getVersion() const {
 	int timestamp = *reinterpret_cast<int*>(0x1400001C0);
-	if (timestamp == 0x5D9DEA53)
+	if (timestamp == 0x5DC03F66)
 		return GameVersion::DX12;
-	else if (timestamp == 0x5D9DEA3E)
+	else if (timestamp == 0x5DC03F3C)
 		return GameVersion::DX11;
 	else
 		return GameVersion::UNK;
 }
 
 //Check IDA databases for notes about how to update offsets. 
-//Current version: 2.71.0
+//Current version: 2.72.0
 GameOffsets::GameOffsets() {
 	switch (getVersion()) {
 	case GameVersion::DX12:
-		offsets.pPushItem = reinterpret_cast<void*>(0x140C24540);
-		offsets.pPushNPCInventoryDetour = reinterpret_cast<void*>(0x140C24AC0);
-		offsets.pPushWorldInventoryDetour = reinterpret_cast<void*>(0x140C24471);
-		offsets.pPushHeroInventoryDetour = reinterpret_cast<void*>(0x1405D7257);
-		offsets.pPushStashInventoryDetour = reinterpret_cast<void*>(0x1405903DA);
-		offsets.pZEntitySceneContext_LoadScene = reinterpret_cast<void**>(0x1416AEE68); 
+		offsets.pPushItem = reinterpret_cast<void*>(0x140C24650);
+		offsets.pPushNPCInventoryDetour = reinterpret_cast<void*>(0x140C24BD0);
+		offsets.pPushWorldInventoryDetour = reinterpret_cast<void*>(0x140C24581);
+		offsets.pPushHeroInventoryDetour = reinterpret_cast<void*>(0x1405D7217);
+		offsets.pPushStashInventoryDetour = reinterpret_cast<void*>(0x14059039A);
+		offsets.pZEntitySceneContext_LoadScene = reinterpret_cast<void**>(0x1416AEE68);
+
 		break;
 	case GameVersion::DX11:
-		offsets.pPushItem = reinterpret_cast<void*>(0x140C249E0);
-		offsets.pPushNPCInventoryDetour = reinterpret_cast<void*>(0x140C24F60);
-		offsets.pPushWorldInventoryDetour = reinterpret_cast<void*>(0x140C24911);
-		offsets.pPushHeroInventoryDetour = reinterpret_cast<void*>(0x1405D7937);
-		offsets.pPushStashInventoryDetour = reinterpret_cast<void*>(0x140590ABA);
-		offsets.pZEntitySceneContext_LoadScene = reinterpret_cast<void**>(0x141693D70); 
+		offsets.pPushItem = reinterpret_cast<void*>(0x140C24AF0);
+		offsets.pPushNPCInventoryDetour = reinterpret_cast<void*>(0x140C25070);
+		offsets.pPushWorldInventoryDetour = reinterpret_cast<void*>(0x140C24A21);
+		offsets.pPushHeroInventoryDetour = reinterpret_cast<void*>(0x1405D78F7);
+		offsets.pPushStashInventoryDetour = reinterpret_cast<void*>(0x140590A7A);
+		offsets.pZEntitySceneContext_LoadScene = reinterpret_cast<void**>(0x141693D70);
 		break;
 	default:
 		MessageBoxA(NULL, "Randomizer doesn't support current game version", "", NULL);
